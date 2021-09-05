fa979a2eb77e0d058763f38444db37fc55388154
edwa3545 <edwa3545@bears.unco.edu>
2021-09-05 16:52:55 -0700

    finish superhero project

--- week2/Superhero/config/urls.py
+++ week2/Superhero/config/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from hero.views import SuperManView, HawkGirlView, FlashView
 
 urlpatterns = [
+    path('', SuperManView.as_view()),
     path('superman', SuperManView.as_view()),
     path('hawkgirl', HawkGirlView.as_view()),
     path('flash', FlashView.as_view()),
