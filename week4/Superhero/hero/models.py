e8d22d1c1d803717885b24cc17c95dc049b53ff5
edwa3545 <edwa3545@bears.unco.edu>
2021-09-18 18:38:12 -0600

    models

--- week4/Superhero/hero/models.py
+++ week4/Superhero/hero/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
+class Hero (models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    image = models.CharField(max_length=200)
+
 # Create your models here.
