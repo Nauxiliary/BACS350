ef11f8728f4f75f1148032f8fbf43a635c7dfd14
edwa3545 <edwa3545@bears.unco.edu>
2021-09-25 18:28:16 -0600

    further week 5

--- week5/Superhero/hero/models.py
+++ week5/Superhero/hero/models.py
@@ -1,10 +1,11 @@
 from os import name
 from django.db import models
 
+
 class Hero(models.Model):
-    name = models.CharField(max_length=200)
-    description = models.CharField(max_length=200)
-    image = models.ImageField()
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    image = models.CharField(max_length=200)
 
     def __str__(self):
         return f'{self.name}, Description: {self.description}'
