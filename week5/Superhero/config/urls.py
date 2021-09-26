fa9258de55b45fb641b9c4064ea3eb00d21b1ee8
edwa3545 <edwa3545@bears.unco.edu>
2021-09-25 22:48:58 -0600

    got views working maybe

--- week5/Superhero/config/urls.py
+++ week5/Superhero/config/urls.py
@@ -15,7 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from hero.views import HeroDetailView, HeroListView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', HeroListView.as_view()),
+    path('<int:pk>/', HeroDetailView.as_view()),
+    path('hero/', HeroListView.as_view()),
+    path('hero/<int:pk>/', HeroDetailView.as_view()),
 ]
--- week5/Superhero/hero/views.py
+++ week5/Superhero/hero/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from hero.models import Hero
+from django.views.generic import DetailView, ListView
 
-# Create your views here.
+
+class HeroListView(ListView):
+    model = Hero
+    template_name = 'hero_list.html'
+
+
+class HeroDetailView(DetailView):
+    model = Hero
+    template_name = 'hero_detail.html'
